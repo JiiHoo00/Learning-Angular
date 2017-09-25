5f833249ae3440c6039b5a0e52636d2d4baf890b
Janne Hanhela <sdsagfsdagewafsfewa@fdsafsdfaesafa.com>
2017-09-25 10:23:59 +0300

    Removed a test, because new UI design made it obsolete

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -24,12 +24,6 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Tour of Heroes');
   }));
-  it('should render Heroes name in a h2 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toContain('Windstorm details!');
-  }));
   it('should have hero object, with id and name', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
