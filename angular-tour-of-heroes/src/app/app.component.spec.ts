ee34b7b6a327381ff1a48e84847939a7df1e5893
Janne Hanhela <sdsagfsdagewafsfewa@fdsafsdfaesafa.com>
2017-09-25 14:01:54 +0300

    Moved the fixture component creation to the beforeEach-function call, to lessen repetition

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -13,56 +13,50 @@ describe('AppComponent', () => {
         FormsModule
       ]
     }).compileComponents();
+    this.fixture = TestBed.createComponent(AppComponent);
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = this.fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'Tour of Heroes'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = this.fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Tour of Heroes');
   }));
   it('should have hero object, with id and name', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = this.fixture.debugElement.componentInstance;
     expect(app.hero.id).toEqual(1);
     expect(app.hero.name).toEqual('Windstorm');
   }));
   it('should render Heroes details in labels', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      const compiled = fixture.debugElement.nativeElement;
+    this.fixture.detectChanges();
+    this.fixture.whenStable().then(() => {
+      const compiled = this.fixture.debugElement.nativeElement;
       expect(compiled.querySelector('div').textContent).toContain('id: 1');
       expect(compiled.querySelector('input').value).toBe('Windstorm');
     });
   }));
   it('should have hero data-array, with ten heroes', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = this.fixture.debugElement.componentInstance;
     expect(app.heroes.length).toEqual(10);
     expect(app.heroes[0].name).toEqual('Mr. Nice');
     expect(app.heroes[0].id).toEqual(11);
   }));
   it('should render heroes data in a list', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
+    this.fixture.detectChanges();
+    const compiled = this.fixture.debugElement.nativeElement;
     expect(compiled.querySelector('span').textContent).toContain(11);
     expect(compiled.querySelector('li').textContent).toContain('Mr. Nice');
   }));
   it('should show selected hero\'s details', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = this.fixture.debugElement.componentInstance;
     app.selectedHero = {
       id: 11,
       name: 'Mr. Nice'
     };
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      const compiled = fixture.debugElement.nativeElement;
+    this.fixture.detectChanges();
+    this.fixture.whenStable().then(() => {
+      const compiled = this.fixture.debugElement.nativeElement;
       expect(compiled.querySelector('input').value).toBe('Mr. Nice');
     });
   }));
