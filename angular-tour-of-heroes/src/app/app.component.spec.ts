fb10ebe1c3704d875670052debf911d91182016d
Janne Hanhela <janne.hanhela@gmail.com>
2017-09-28 15:03:46 +0300

    Changed hero-service to asynchronous (Promise), so that the change to real data is contained

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -16,6 +16,7 @@ describe('AppComponent', () => {
       ]
     }).compileComponents();
     this.fixture = TestBed.createComponent(AppComponent);
+    this.fixture.detectChanges(); // makes sure that the ngOnInit is run in the component, so that the data is loaded
   }));
   it('should create the app', async(() => {
     const app = this.fixture.debugElement.componentInstance;
@@ -31,17 +32,22 @@ describe('AppComponent', () => {
     expect(app.hero.name).toEqual('Windstorm');
   }));
   it('should have hero data-array, with ten heroes', async(() => {
-    this.fixture.detectChanges(); // this causes the ngOnInit to run in the component
-    const app = this.fixture.debugElement.componentInstance;
-    expect(app.heroes.length).toEqual(10);
-    expect(app.heroes[0].name).toEqual('Mr. Nice');
-    expect(app.heroes[0].id).toEqual(11);
+    this.fixture.whenStable().then(() => { // so that service has gotten the mock-data
+      const app = this.fixture.debugElement.componentInstance;
+      expect(app.heroes.length).toEqual(10);
+      expect(app.heroes[0].name).toEqual('Mr. Nice');
+      expect(app.heroes[0].id).toEqual(11);
+    });
+
   }));
   it('should render heroes data in a list', async(() => {
-    this.fixture.detectChanges();
-    const compiled = this.fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('span').textContent).toContain(11);
-    expect(compiled.querySelector('li').textContent).toContain('Mr. Nice');
+    this.fixture.whenStable().then(() => { // so that service has gotten the mock-data
+      this.fixture.detectChanges();
+      const compiled = this.fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('span').textContent).toContain(11);
+      expect(compiled.querySelector('li').textContent).toContain('Mr. Nice');
+    });
+
   }));
   it('should show selected hero\'s details', async(() => {
     const app = this.fixture.debugElement.componentInstance;
@@ -55,24 +61,23 @@ describe('AppComponent', () => {
       expect(compiled.querySelector('input').value).toBe('Mr. Nice');
     });
   }));
-  it('should show clicked hero\'s details', async(() => {
-    this.fixture.detectChanges();
+  it('should show clicked hero\'s details', fakeAsync(() => { // not completely sure how this works, but this goes like this:
+    this.fixture.detectChanges(); // updates the view to current situation
     const compiled = this.fixture.debugElement.nativeElement;
-    compiled.querySelectorAll('.badge')[0].click();
-    this.fixture.detectChanges();
-    this.fixture.whenStable().then(() => {
-      const compiledAfterClick = this.fixture.debugElement.nativeElement;
-      expect(compiledAfterClick.querySelectorAll('input')[0].value).toBe('Mr. Nice');
-    });
+    compiled.querySelectorAll('.badge')[0].click(); // clicks the hero
+    this.fixture.detectChanges(); // updates the view, so that the click is detected
+    tick(); // ticks the time forward
+    this.fixture.detectChanges(); // updates the view to current situation
+    const compiledAfterClick = this.fixture.debugElement.nativeElement;
+    expect(compiledAfterClick.querySelectorAll('input')[0].value).toBe('Mr. Nice');
   }));
-  it('should apply css class selected to the selected hero', async(() => {
+  it('should apply css class selected to the selected hero', fakeAsync(() => {
     this.fixture.detectChanges();
     const compiled = this.fixture.debugElement.nativeElement;
     compiled.querySelectorAll('.badge')[0].click();
+    tick();
     this.fixture.detectChanges();
-    this.fixture.whenStable().then(() => {
-      const compiledAfterClick = this.fixture.debugElement.nativeElement;
-      expect(compiledAfterClick.querySelectorAll('li')[0].className).toContain('selected');
-    });
+    const compiledAfterClick = this.fixture.debugElement.nativeElement;
+    expect(compiledAfterClick.querySelectorAll('li')[0].className).toContain('selected');
   }));
 });
