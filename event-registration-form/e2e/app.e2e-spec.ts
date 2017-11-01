bea54900dde7353496dbd5e8ea2672733006fe2f
Janne Hanhela <janne.hanhela@gmail.com>
2017-11-01 10:14:30 +0200

    Updated e2e-spec.ts so that some end to end tests can be added in the future

--- event-registration-form/e2e/app.e2e-spec.ts
+++ event-registration-form/e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('event-registration-form App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display header text', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Event Registration Form');
   });
 });
